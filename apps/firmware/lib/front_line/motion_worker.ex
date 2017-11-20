6610e00324c29c09953a6c2794e66700d9fb1077
Dorian Karter <dkarter@gmail.com>
2017-11-19 21:28:38 -0600

    Debounce motion sensor rising signal

--- apps/firmware/lib/front_line/motion_worker.ex
+++ apps/firmware/lib/front_line/motion_worker.ex
@@ -5,6 +5,7 @@ defmodule FrontLine.MotionWorker do
 
   @input_pin Application.get_env(:front_line, :input_pin, 20)
   @output_pin Application.get_env(:front_line, :output_pin, 26)
+  @debounce_time 4_000
 
   def start_link do
     GenServer.start_link(__MODULE__, [])
@@ -22,14 +23,29 @@ defmodule FrontLine.MotionWorker do
   end
 
   def handle_info({:gpio_interrupt, pin, :rising}, state) do
-    GPIO.write(state[:output_pid], 1)
+    if state[:debounce_timer] do
+      Logger.debug "========================= debounced"
+      Process.cancel_timer(state[:debounce_timer])
+    else
+      GPIO.write(state[:output_pid], 1)
+    end
+
+    timer = Process.send_after(self(), :motion_stopped, @debounce_time)
+    state = Map.put(state, :debounce_timer, timer)
     Logger.debug "MotionWorker got :rising signal on #{pin}"
+
     {:noreply, state}
   end
 
   def handle_info({:gpio_interrupt, pin, :falling}, state) do
-    GPIO.write(state[:output_pid], 0)
     Logger.debug "MotionWorker got :falling signal on #{pin}"
+    {:noreply, state}
+  end
+
+  def handle_info(:motion_stopped, state) do
+    Logger.debug "--------------------------- motion stopped"
+    GPIO.write(state[:output_pid], 0)
+    state = Map.delete(state, :debounce_timer)
     {:noreply, state}
   end
 end
